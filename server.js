758a2b955f55ca4a69eb89c4fd68d078d7ba9ecc
Bhyland <blakehyland95@gmail.com>
2021-10-15 10:38:35 -0600

    added inquirer and initial questions

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const db = require('./db/connection');
+const mysql = require('mysql2')
+const inquirer = require('inquirer'); 
+const cTable = require('console.table'); 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,5 +17,34 @@ db.connect(err => {
     console.log('Database connected.');
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
+        welcome();
     });
 });
+
+welcome = () => {
+    console.log('************************');
+    console.log('*                      *');
+    console.log('*   Employee Tracker   *');
+    console.log('*                      *');
+    console.log('************************');
+promptUser()
+}
+
+// inquirer prompt for first action
+const promptUser = () => {
+    inquirer.prompt ([
+      {
+        type: 'list',
+        name: 'choices', 
+        message: 'What would you like to do?',
+        choices: ['View all departments', 
+                  'View all roles', 
+                  'View all employees', 
+                  'Add a department', 
+                  'Add a role', 
+                  'Add an employee', 
+                  'Update an employee role'
+                ]
+      }
+    ])
+}
